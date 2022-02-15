714d073c97c268228eb0d73a7a420f79b9655cd4
unknown <asthaagarwal203@gmail.com>
2022-02-15 19:35:00 +0530

    [Astha] Add: SwapTwoNo

--- FlipCoin/Program.cs
+++ FlipCoin/Program.cs
@@ -10,12 +10,13 @@ namespace FlipCoin
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Enter a no");
-            int no = Convert.ToInt32(Console.ReadLine());
-            if (no % 2 == 0)
-                Console.WriteLine("Even No");
-            else
-                Console.WriteLine("Odd No");
+            Console.WriteLine("Enter Two numbers");
+            int no1 = Convert.ToInt32(Console.ReadLine());
+            int no2 = Convert.ToInt32(Console.ReadLine());
+            int temp = no1;
+            no1 = no2;
+            no2= temp;
+            Console.WriteLine("No1 is " + no1 + "\nNo2 is " + no2);
         }
     }
 }
