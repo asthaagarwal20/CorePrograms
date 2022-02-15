23060a9ef420285bd08d885f1340bfa9afabd941
unknown <asthaagarwal203@gmail.com>
2022-02-15 19:30:47 +0530

    [Astha] Add: EvenOdd

--- FlipCoin/Program.cs
+++ FlipCoin/Program.cs
@@ -10,19 +10,12 @@ namespace FlipCoin
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Enter the value of n between 0 and 31");
-            int n = Convert.ToInt32(Console.ReadLine());
-            int year, ans = 1;
-            for (int i = 0; i < n; i++)
-                ans = ans * 2;
-            year = ans;
-            Console.WriteLine(ans);
-            if (year % 400 == 0)
-                Console.WriteLine("Leap year");
-            else if (year % 4 == 0 && year % 100 != 0)
-                Console.WriteLine("Leap year");
+            Console.WriteLine("Enter a no");
+            int no = Convert.ToInt32(Console.ReadLine());
+            if (no % 2 == 0)
+                Console.WriteLine("Even No");
             else
-                Console.WriteLine("Not Leap Year");
+                Console.WriteLine("Odd No");
         }
     }
 }
